5e7f7ca30985f17827dcdaefb1edd1e2fc76aa9c
Elfana, A | Elfaael <elfaael12@gmail.com>
2023-07-22 14:47:25 +0700

    Initial Commit

---
+++ MyNews/MyNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNewsApp.swift
+//  MyNews
+//
+//  Created by Elfana Anamta Chatya on 22/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MyNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
